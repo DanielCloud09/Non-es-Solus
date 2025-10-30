793c9739c2c3b5d9362cbf2606aaba7330c879f6
DanielCloud09 <daniel.vera@estudiantes.colegiotgs.cl>
2025-10-30 13:22:03 -0300

    inicio.py

--- inicio.py
+++ inicio.py
@@ -3,7 +3,7 @@ import streamlit as st
 #linea para escribir un titulo
 st.title("TEST DE ALTERNOS")
 #linea de subtitulo
-st.markdown("Proyecto inspirado en el universo del Mandela Catalogue, no apto para asustadisos, niños, tontitos, personas de bajo intelecto, enfermos cardiacos, enfermos terminales y loquitos, ademas de estar dirigido especialmente para las personas(especificamente danieles, matias, maximilianos y gonzalos) de la region metropólitana pero tambien para el resto de Chile como precaucion. ")
+st.markdown("Proyecto inspirado en el universo del Mandela Catalogue, no apto para asustadisos, niños, tontitos, personas de bajo intelecto, enfermos cardiacos, enfermos terminales y loquitos, ademas de estar dirigido especialmente para las personas(especificamente danieles, matias, maximilianos y gonzalos) de la region metropólitana pero tambien para el resto de Chile como precaucion, ademas de tener un apartado de conocimiento sobre la grasa para los interesados que hayan pasado el test de alternos. ")
 
 # Imagen de portada
 st.image("mand.jpg", caption="**Ellos se ven como nosotros...**")
@@ -27,13 +27,15 @@ st.subheader("Objetivo del Proyecto")
 st.write(
     """
     El objetivo de esta aplicación es simular un **entrenamiento del Departamento de Defensa** del Condado de Mandela,
-    diseñado para identificar **Alternos**: entidades que **imitan la forma humana** pero carecen de empatía y racionalidad.
+    diseñado para identificar **Alternos**: entidades que **imitan la forma humana** pero carecen de empatía y racionalidad ademas de un apartado sobre la grasa para los que hayan pasado.
 
     En versiones futuras, podrás:
     - Realizar un **test visual** con fotografías de humanos y alternos.
     - Escuchar **grabaciones distorsionadas** y decidir si provienen de una persona o una entidad.
     - Participar en **simulaciones interactivas** donde tus decisiones determinarán tu destino.
+    - Calcular tu porcentaje de grasa y aasegurar por las dudas de que no eres un alterno.
     """
 )
+
 
 
